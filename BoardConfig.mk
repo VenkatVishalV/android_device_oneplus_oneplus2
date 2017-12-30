b14695eff8eedcc78aa3d42668bd2a5d4fcf7eef
Christopher N. Hesse <raymanfx@gmail.com>
2017-12-30 11:25:14 +0100

    oneplus2: Use separate fstab for recovery * otapackage requires /system to be present in the   fstab, but early mounting /system means that it   cannot be in the Android fstab, otherwise fs_mgr   will fail to mount it and subsequently fail to boot.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -210,7 +210,7 @@ WITH_DEXPREOPT := false
 DONT_DEXPREOPT_PREBUILTS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/recovery.fstab
 
 # inherit from the proprietary version
 -include vendor/oneplus/oneplus2/BoardConfigVendor.mk
